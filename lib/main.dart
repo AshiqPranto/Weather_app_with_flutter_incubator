4810c9d53d2c50216ced3764c16f7131b7382b69
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-11-12 05:07:12 +0600

    day 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 void main()
 {
   runApp(MyApp());
@@ -11,14 +15,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int a = 5;
-  TextEditingController cityNameController = new TextEditingController();
-  void getWeather()
+  /*
+  *
+
+{
+
+  "weather": [
+    {
+      "main": "Rain",
+      "description": "moderate rain",
+      "icon": "10d"
+    }
+  ],
+  "main": {
+    "temp": 298.48,
+    "feels_like": 298.74,
+  },
+  "name": "Zocca",
+  "cod": 200
+}
+
+                        */
+
+  var currentCity ;
+  var temperature;
+  var description;
+
+  void getWeather() async
   {
     print("clicked");
     String cityName = cityNameController.text;
-    print(cityName);
+    final queryparameter = {
+      "q": cityName,
+      "appid": "476d7f2559919d457197305729d0b1aa"
+    };
+    Uri uri = new Uri.https("api.openweathermap.org","/data/2.5/weather",queryparameter);
+    final jsonData = await get(uri);
+    final json = jsonDecode(jsonData.body);
+
+    setState(() {
+      currentCity = json["name"];
+      temperature = json["main"]["temp"];
+      description = json["weather"][0]["main"];
+    });
   }
+  TextEditingController cityNameController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,31 +71,31 @@ class _MyAppState extends State<MyApp> {
           body:
           Center(
             child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Currently in ",
-                style: TextStyle(fontSize: 30),
-              ),
-              Text("25\u00B0",
-                style: TextStyle(fontSize: 50),
-              ),
-              Text("Rainy",
-                style: TextStyle(fontSize: 30),
-              ),
-              SizedBox(
-                width: 200,
-                child: TextField(
-                  controller: cityNameController,
-                  textAlign: TextAlign.center,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Currently in "+(currentCity == null? "Loading" : currentCity),
+                  style: TextStyle(fontSize: 30),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: getWeather,
-                  child: Text("search")
-              )
-            ],
-          )
+                Text((temperature == null? "Loading": (temperature-273).toStringAsFixed(2)).toString()+"\u00B0",
+                  style: TextStyle(fontSize: 50),
+                ),
+                Text((description == null ? "Loading" : description),
+                  style: TextStyle(fontSize: 30),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: TextField(
+                    controller: cityNameController,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: getWeather,
+                    child: Text("search")
+                )
+              ],
+            )
         )
       ),
     );
